f9f1161f116333ee19f96a8c25bf2ec6551b28bf
alexkudzul <alexkudzul@gmail.com>
2023-03-30 17:45:58 -0600

    03 - Vinculacion de datos

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -7,9 +7,13 @@ use Livewire\Component;
 
 class ShowPosts extends Component
 {
+    public $search;
+
     public function render()
     {
-        $posts = Post::all();
+        $posts = Post::where('title', 'LIKE', '%' . $this->search . '%')
+            ->orWhere('content', 'LIKE', '%' . $this->search . '%')
+            ->get();
 
         // Por defecto se utiliza el layout app.blade.php
         return view('livewire.show-posts', compact('posts'));
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -5,5 +5,64 @@
         </h2>
     </x-slot>
 
-    {{ $posts }}
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <x-table>
+            <div class="px-6 py-4">
+                <x-input class="w-full" placeholder="Escriba lo que quiera buscar" type="text" wire:model="search" />
+            </div>
+
+            @if (count($posts))
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                class="w-24 cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                ID
+                            </th>
+                            <th scope="col"
+                                class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Title
+                            </th>
+                            <th scope="col"
+                                class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Content
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach ($posts as $item)
+                            <tr>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $item->id }}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $item->title }}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $item->content }}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="px-6 py-4">
+                    No existe ningún registro coincidente
+                </div>
+            @endif
+        </x-table>
+    </div>
+
 </div>
