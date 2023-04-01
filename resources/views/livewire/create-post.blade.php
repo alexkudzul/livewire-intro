540fd61aaef289a1c405cf5e7f6e0e50cfda7a86
alexkudzul <alexkudzul@gmail.com>
2023-04-01 09:06:14 -0600

    05 - Metodos magicos y modales

---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <x-danger-button wire:click="$set('open', true)">
+        Crear nuevo Post
+    </x-danger-button>
+
+    <x-dialog-modal wire:model="open">
+        <x-slot name="title">
+            Crear nuevo Post
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mb-4">
+                <x-label value="Título del Post" />
+                <x-input class="w-full" type="text" wire:model.defer="title" />
+            </div>
+
+            <div class="mb-4">
+                <x-label value="Contenido del Post" />
+                <textarea class="form-control w-full" rows="6" wire:model.defer="content"></textarea>
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-secondary-button wire:click="$set('open', false)" class="mr-3">
+                Cancelar
+            </x-secondary-button>
+
+            <x-danger-button wire:click="save">
+                Crear Post
+            </x-danger-button>
+        </x-slot>
+    </x-dialog-modal>
+</div>
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -7,8 +7,9 @@
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <x-table>
-            <div class="px-6 py-4">
-                <x-input class="w-full" placeholder="Escriba lo que quiera buscar" type="text" wire:model="search" />
+            <div class="px-6 py-4 flex items-center">
+                <x-input class="flex-1 mr-4" placeholder="Escriba lo que quiera buscar" type="text" wire:model="search" />
+                @livewire('create-post')
             </div>
 
             @if (count($posts))
